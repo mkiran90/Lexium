e8f4feb097511848a867c3234b552406c099ce59
SabihAli <sabih.ali.9000@gmail.com>
2024-12-23 01:11:39 +0500

    minor change

--- code/Ranking/Semantic.py
+++ code/Ranking/Semantic.py
@@ -13,15 +13,6 @@ document_metadata = DocumentMetadata()
 word_embedding = WordEmbedding()
 lexicon = Lexicon()
 
-def encode_document(model, document, chunk_size=1024):
-    # Split document into chunks of chunk_size words
-    words = document.split()
-    chunks = [' '.join(words[i:i + chunk_size]) for i in range(0, len(words), chunk_size)]
-    chunk_embeddings = model.encode(chunks, convert_to_tensor=True)
-
-    document_embedding = chunk_embeddings.mean(dim=0)
-    return document_embedding
-
 def cosine_similarity(a, b):
 
     # Compute the dot product of a and b
